647735a75127ae54303a6a7a8cf5c4a897c36409
avila-r <avila.dev@outlook.com>
2024-06-05 12:43:48 -0300

    feat: implement new operations

--- src/main/kotlin/com/avila/kopportunities/repository/JobRepository.kt
+++ src/main/kotlin/com/avila/kopportunities/repository/JobRepository.kt
@@ -2,11 +2,20 @@ package com.avila.kopportunities.repository
 
 import com.avila.kopportunities.model.Job
 
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.ListCrudRepository
 import org.springframework.stereotype.Repository
 
+import java.math.BigDecimal
 import java.util.UUID
 
-@Repository interface JobRepository : CrudRepository<Job, UUID> {
+@Repository interface JobRepository : ListCrudRepository<Job, UUID> {
     fun findJobById(id: UUID?): Job?
+    fun findAllByTitle(title: String?): List<Job>?
+    fun findAllByDescription(description: String?): List<Job>?
+    fun findAllByCompany(company: String?): List<Job>?
+    fun findAllByLocation(location: String?): List<Job>?
+    fun findAllByType(type: String?): List<Job>?
+    fun findAllBySalary(salary: BigDecimal?): List<Job>?
+    fun findAllByRequirements(requirements: String?): List<Job>?
+    fun findAllByResponsibilities(responsibilities: String?): List<Job>?
 }
