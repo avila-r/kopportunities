f336ea985d91eb8ab14904410a2c8faad67073e3
avila-r <avila.dev@outlook.com>
2024-06-05 12:43:41 -0300

    feat: implement new operations

--- src/main/kotlin/com/avila/kopportunities/service/JobService.kt
+++ src/main/kotlin/com/avila/kopportunities/service/JobService.kt
@@ -1,5 +1,6 @@
 package com.avila.kopportunities.service
 
+import com.avila.kopportunities.exception.JobNotFoundException
 import com.avila.kopportunities.model.Job
 import com.avila.kopportunities.repository.JobRepository
 
@@ -7,14 +8,50 @@ import jakarta.transaction.Transactional
 
 import org.springframework.stereotype.Service
 
+import java.math.BigDecimal
 import java.util.UUID
 
 @Service class JobService( private val repository: JobRepository ) {
 
-    @Transactional fun insertJob(job: Job?): Job =
-        repository.save(job ?: throw Exception()) // Todo: custom exception
+    @Transactional fun insert(job: Job?): Job =
+        repository.save(job ?: throw JobNotFoundException())
 
-    fun getJob(id: UUID): Job =
-        repository.findJobById(id) ?: throw Exception() // Todo: custom exception
+    @Transactional fun update(job: Job?): Job =
+        repository.save(repository.findJobById(job?.id) ?: throw JobNotFoundException())
+
+    fun getById(id: UUID): Job =
+        repository.findJobById(id) ?: throw JobNotFoundException()
+
+    fun getByTitle(title: String): List<Job> =
+        repository.findAllByTitle(title) ?: throw JobNotFoundException()
+
+    fun getByCompany(company: String): List<Job> =
+        repository.findAllByCompany(company) ?: throw JobNotFoundException()
+
+    fun getByLocation(location: String): List<Job> =
+        repository.findAllByLocation(location) ?: throw JobNotFoundException()
+
+    fun getByType(type: String): List<Job> =
+        repository.findAllByType(type) ?: throw JobNotFoundException()
+
+    fun getBySalary(salary: BigDecimal): List<Job> =
+        repository.findAllBySalary(salary) ?: throw JobNotFoundException()
+
+    fun getByRequirements(requirements: String): List<Job> =
+        repository.findAllByRequirements(requirements) ?: throw JobNotFoundException()
+
+    fun getByResponsibilities(responsibilities: String): List<Job> =
+        repository.findAllByResponsibilities(responsibilities) ?: throw JobNotFoundException()
+
+    fun listAll(): List<Job> =
+        repository.findAll()
+
+    fun deleteById(id: UUID): Boolean =
+        try {
+            repository.deleteById(id)
+            true
+        } catch (e: Exception) {
+            false
+        }
 
 }
