4bef17e23b935b7f194e3397c92881688f0d053e
avila-r <avila.dev@outlook.com>
2024-06-05 12:44:02 -0300

    feat: implement new endpoints

--- src/main/kotlin/com/avila/kopportunities/controller/JobController.kt
+++ src/main/kotlin/com/avila/kopportunities/controller/JobController.kt
@@ -1,6 +1,7 @@
 package com.avila.kopportunities.controller
 
-import com.avila.kopportunities.model.Job
+import com.avila.kopportunities.model.JobRequestDTO
+import com.avila.kopportunities.model.JobResponseDTO
 import com.avila.kopportunities.service.JobService
 
 import org.springframework.http.HttpStatus
@@ -14,68 +15,142 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 import java.math.BigDecimal
-import java.time.LocalDate
 import java.util.UUID
 
 @RequestMapping("/api/v1/jobs")
 @RestController class JobController ( private val service: JobService ) {
 
-    data class Request(
-        val title: String,
-        val description: String,
-        val company: String,
-        val location: String,
-        val type: String,
-        val salary: BigDecimal,
-        val requirements: String,
-        val responsibilities: String
-    ) {
-        fun build():Job =
-            Job(
-                title = this.title,
-                description = this.description,
-                company = this.company,
-                location = this.location,
-                type = this.type,
-                salary = this.salary,
-                requirements = this.requirements,
-                responsibilities = this.responsibilities
-            )
-    }
-
     data class Response(
-        val id: UUID?,
-        val lastModified: LocalDate?,
-        val status: String?,
-        val title: String,
-        val description: String,
-        val company: String,
-        val location: String,
-        val type: String,
-        val salary: BigDecimal,
-        val requirements: String,
-        val responsibilities: String
+        val code: Int,
+        val status: String,
+        val message: String
     )
 
     @GetMapping("/id/{id}")
-    fun getJob(@PathVariable id: String):http<Response> =
+    fun getJob(@PathVariable id: String):http<JobResponseDTO> =
         try {
             http
                 .status(HttpStatus.OK)
-                .body(service.getJob(UUID.fromString(id)).build())
+                .body(service.getById(UUID.fromString(id)).build())
+        } catch (e: Exception) {
+            println(e)
+            ResponseEntity.badRequest().build()
+        }
+
+    @GetMapping
+    fun getAllJobs():http<List<JobResponseDTO>> =
+        try {
+            http
+                .status(HttpStatus.OK)
+                .body(service.listAll().map { it.build() })
+        } catch (e: Exception) {
+            println(e)
+            ResponseEntity.badRequest().build()
+        }
+
+    @GetMapping("/title/{title}")
+    fun getJobByTitle(@PathVariable title: String):http<List<JobResponseDTO>> =
+        try {
+            http
+                .status(HttpStatus.OK)
+                .body(service.getByTitle(title).map { it.build() })
+        } catch (e: Exception) {
+            println(e)
+            ResponseEntity.badRequest().build()
+        }
+
+    @GetMapping("/company/{company}")
+    fun getJobByCompany(@PathVariable company: String):http<List<JobResponseDTO>> =
+        try {
+            http
+                .status(HttpStatus.OK)
+                .body(service.getByCompany(company).map { it.build() })
+        } catch (e: Exception) {
+            println(e)
+            ResponseEntity.badRequest().build()
+        }
+
+    @GetMapping("/location/{location}")
+    fun getJobByLocation(@PathVariable location: String):http<List<JobResponseDTO>> =
+        try {
+            http
+                .status(HttpStatus.OK)
+                .body(service.getByLocation(location).map { it.build() })
+        } catch (e: Exception) {
+            println(e)
+            ResponseEntity.badRequest().build()
+        }
+
+    @GetMapping("/type/{type}")
+    fun getJobByType(@PathVariable type: String):http<List<JobResponseDTO>> =
+        try {
+            http
+                .status(HttpStatus.OK)
+                .body(service.getByType(type).map { it.build() })
+        } catch (e: Exception) {
+            println(e)
+            ResponseEntity.badRequest().build()
+        }
+
+    @GetMapping("/salary/{salary}")
+    fun getJobBySalary(@PathVariable salary: BigDecimal):http<List<JobResponseDTO>> =
+        try {
+            http
+                .status(HttpStatus.OK)
+                .body(service.getBySalary(salary).map { it.build() })
+        } catch (e: Exception) {
+            println(e)
+            ResponseEntity.badRequest().build()
+        }
+
+    @GetMapping("/requirements/{requirements}")
+    fun getJobByRequirements(@PathVariable requirements: String):http<List<JobResponseDTO>> =
+        try {
+            http
+                .status(HttpStatus.OK)
+                .body(service.getByRequirements(requirements).map { it.build() })
+        } catch (e: Exception) {
+            println(e)
+            ResponseEntity.badRequest().build()
+        }
+
+    @GetMapping("/responsibilities/{responsibilities}")
+    fun getJobByResponsibilities(@PathVariable responsibilities: String):http<List<JobResponseDTO>> =
+        try {
+            http
+                .status(HttpStatus.OK)
+                .body(service.getByResponsibilities(responsibilities).map { it.build() })
+        } catch (e: Exception) {
+            println(e)
+            ResponseEntity.badRequest().build()
+        }
+
+    @PostMapping("/delete/{id}")
+    fun deleteJob(@PathVariable id: String):http<Response> =
+        try {
+            http
+                .status(HttpStatus.OK)
+                .body(
+                    if (service.deleteById(UUID.fromString(id))) {
+                        Response(200, "OK", "Job deleted successfully")
+                    } else {
+                        Response(400, "Bad Request", "Job not found")
+                    }
+                )
         } catch (e: Exception) {
             println(e)
             ResponseEntity.badRequest().build()
         }
 
     @PostMapping
-    fun postJob(@RequestBody request: Request):http<Response> =
+    fun postJob(@RequestBody request: JobRequestDTO):http<JobResponseDTO> =
         try {
             http
                 .status(HttpStatus.CREATED)
-                .body(service.insertJob(request.build()).build())
+                .body(service.insert(request.build()).build())
         } catch (e: Exception) {
             println(e)
             ResponseEntity.badRequest().build()
         }
+
 }
