01f32a2bfae6a1d3b874093480608b847e940e78
avila-r <avila.dev@outlook.com>
2024-06-05 23:15:37 -0300

    feat: implement handler

--- src/main/kotlin/com/avila/kopportunities/exception/JobNotFoundException.kt
+++ src/main/kotlin/com/avila/kopportunities/exception/JobNotFoundException.kt
@@ -1,13 +1,16 @@
 package com.avila.kopportunities.exception
 
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.http.ResponseEntity as http
 import org.springframework.web.bind.annotation.ExceptionHandler
 
 class JobNotFoundException : Exception("Job not found")
 
-@ExceptionHandler( JobNotFoundException::class )
-fun handleJotNotFoundException(e: JobNotFoundException): http<HttpErrorResponse> =
-    http
-        .status(HttpStatus.NOT_FOUND)
-        .body(HttpErrorResponse(code = 404, status = HttpStatus.NOT_FOUND.toString(), message = e.message ?: "Job not found"))
+@ControllerAdvice class JobNotFoundExceptionHandler {
+    @ExceptionHandler( JobNotFoundException::class )
+    fun handleJobNotFoundException(e: JobNotFoundException): http<HttpErrorResponse> =
+        http
+            .status(HttpStatus.NOT_FOUND)
+            .body(HttpErrorResponse(status = HttpStatus.NOT_FOUND.toString(), message = e.message ?: "Job not found"))
+}
