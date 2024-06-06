384fb66ea1bb077b53a984c2a42364880a28c806
avila-r <avila.dev@outlook.com>
2024-06-06 02:03:42 -0300

    fix gradlew reference

--- src/main/kotlin/com/avila/kopportunities/KopportunitiesApplication.kt
+++ src/main/kotlin/com/avila/kopportunities/KopportunitiesApplication.kt
@@ -3,8 +3,7 @@ package com.avila.kopportunities
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-class KopportunitiesApplication
+@SpringBootApplication class KopportunitiesApplication
 
 fun main(args: Array<String>) {
     runApplication<KopportunitiesApplication>(*args)
